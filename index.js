f32200005c5e88c53295ba8f60c343ff5c28599b
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-06-27 21:35:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+let configObj = {
+ 
+  };
+
+
+function submitData(inputName, inputEmail) {
+
+    return fetch("http://localhost:3000/users", { 
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+        },
+        body: JSON.stringify({name: inputName, email: inputEmail})
+        })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        document.body.textContent = object.id;
+    })
+    .catch(function(error) {
+        alert("Bad things! Ragnarők!");
+        document.body.textContent = error.message;
+    });
+
+};
